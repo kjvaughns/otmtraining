7ef9eaf6c3428ddb853139203790142bfad060fa
kjvaughns <kjvaughns13@gmail.com>
2024-06-08 14:43:43 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false,
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        green: '#008000',
-        black: '#000000',
-        white: '#ffffff',
+        primary: '#008000', // Green
+        secondary: '#000000', // Black
+        accent: '#ffffff', // White
       },
     },
   },
